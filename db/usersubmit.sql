db563ed75637a5fcd60f4a0b66fae76aadbb8081
Momoe Murakata <mox2e@momoe.lan>
2013-12-11 07:20:44 -0800

    set usersubmited questions to not evaluated yet

--- db/usersubmit.sql
+++ db/usersubmit.sql
@@ -1,47 +1,47 @@
 INSERT INTO question (qid, subtopicid, media, question, answer1, answer2, answer3, answer4, correct_answer, submitedby, evaluatedby) VALUES
 ( "q6030", "st6", "q6030.jpg",
  "Where does the story of the cartoon take place?",
- "London", "Paris", "Geneva", "Rome", "2", "user6", "admin");
+ "London", "Paris", "Geneva", "Rome", "2", "user6", "");
 
 INSERT INTO question (qid, subtopicid, media, question, answer1, answer2, answer3, answer4, correct_answer, submitedby, evaluatedby) VALUES
 ( "q6031", "st6", "",
 "How many kittens does Duchess have?", 
 "One", "Two", "Three", "Four",
- "3", "user6", "admin");
+ "3", "user6", "");
 
 INSERT INTO question (qid, subtopicid, media, question, answer1, answer2, answer3, answer4, correct_answer, submitedby, evaluatedby) VALUES
 ( "q6032", "st6", "",
 "The names of the kittens are...", 
 "Marie, Berlioz and Toulouse", "Marie, Berlioz and Karl", "Marie, Lance and Toulouse", "Megan, Lance and Toulouse",
- "1", "user6", "admin");
+ "1", "user6", "");
 
 INSERT INTO question (qid, subtopicid, media, question, answer1, answer2, answer3, answer4, correct_answer, submitedby, evaluatedby) VALUES
 ( "q6033", "st6", "",
 "What does Madame, the owner of all the cats, want to do with her property after she is dead?", 
 "leave it for Michelle, her sister", "leave it to Edgar, her butler", "leave it to her cats", "wants to have it burned",
- "3", "user6", "admin");
+ "3", "user6", "");
 
 INSERT INTO question (qid, subtopicid, media, question, answer1, answer2, answer3, answer4, correct_answer, submitedby, evaluatedby) VALUES
 ( "q6034", "st6", "q6034.jpg",
 "When Edgar gives Duchess and her kittens the milk with sleeping pills in it, what does he call it?", 
 "Creme de la creme a la Edgar ", "Hot n creamy delicious milk ", "Edgar's special treat for beautiful cats ", "Yum yum yummy milky treat",
- "1", "user6", "admin");
+ "1", "user6", "");
 
 INSERT INTO question (qid, subtopicid, media, question, answer1, answer2, answer3, answer4, correct_answer, submitedby, evaluatedby) VALUES
 ( "q6035", "st6", "q6035.jpg",
 "When Duchess and her kittens are taken away and thrown by Edgar, whom do they meet?", 
 "Abraham DeLacey", "Thomas O'Malley", "Michael Rickman", "Matthew Crossman",
- "2", "user7", "admin");
+ "2", "user7", "");
 
 INSERT INTO question (qid, subtopicid, media, question, answer1, answer2, answer3, answer4, correct_answer, submitedby, evaluatedby) VALUES
 ( "q6036", "st6", "q6036.jpg",
 "When all the cats make a train and walk on the railway track, who falls off?", 
 "Berlioz", "Toulouse", "Duchess", "Marie",
- "4", "user2", "admin");
+ "4", "user2", "");
 
 INSERT INTO question (qid, subtopicid, media, question, answer1, answer2, answer3, answer4, correct_answer, submitedby, evaluatedby) VALUES
 ( "q6037", "st6", "q6037.jpg",
 "When Thomas, Duchess and the kittens are in the back of someone's car, Thomas tells the kittens to do a few things in order to get a treat. What is one of the things he tells them to do?", 
 "clap thrice", "pull their tails", "wiggle their noses", "jump about",
- "3", "user2", "admin");
+ "3", "user2", "");
 
